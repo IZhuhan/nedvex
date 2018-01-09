906be61bf4adb471f12cdbd4892dff6b245854f6
IZhuhan <zhugan.irina@gmail.com>
2018-01-09 16:33:56 +0200

    changed init.js

--- js/init.js
+++ js/init.js
@@ -1,13 +1,6 @@
-var slider = $('#dg-container');
-var sld = function() {
-  if ($(window).width() > 838) {
-    slider.gallery({
-        autoplay: true,
-        interval: 5000
-    });
-  } else {
-    slider.gallery.destroy;
-  }
-};
-sld();
-$(window).resize(sld);
+$(function() {
+  $('#dg-container').gallery({
+    autoplay: true,
+    interval: 5000
+  });
+});
